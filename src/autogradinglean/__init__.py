b0970f41916fb7aa59ee03f22375b019285ce24e
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-11-02 22:14:27 +0000

    fix linter warning concerning unused import

--- src/autogradinglean/__init__.py
+++ src/autogradinglean/__init__.py
@@ -1,4 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present Gihan Marasingha <gihan.marasingha@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-from .github_work import GitHubClassroom, GitHubClassroomManager
+from autogradinglean.github_work import GitHubClassroom, GitHubClassroomManager
+
+__all__ = ["GitHubClassroom", "GitHubClassroomManager"]
