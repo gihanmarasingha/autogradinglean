188b41c7261eedcf8075bbc83f85ec38e5dc2210
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-11-11 20:09:33 +0000

    fix command method name called by GitHubClassroomManager

--- src/autogradinglean/base/manager.py
+++ src/autogradinglean/base/manager.py
@@ -10,15 +10,15 @@ from autogradinglean.base.base import GitHubClassroomBase
 class GitHubClassroomManager(GitHubClassroomBase):
     """A class for representing all classrooms for the current user"""
     def __init__(self):
-        self.df_classrooms = pd.DataFrame()
+        self.classrooms = pd.DataFrame()
         self.fetch_classrooms()
 
     def fetch_classrooms(self):
         """Get all classroom data"""
         command = "/classrooms"
-        output = self._run_gh_api_command(command)
+        output = self._run_gh_api_command_base(command)
         try:
             classrooms_data = json.loads(output)
-            self.df_classrooms = pd.DataFrame(classrooms_data)
+            self.classrooms = pd.DataFrame(classrooms_data)
         except json.JSONDecodeError as e:
             logging.error("Failed to decode JSON: %s", e)
